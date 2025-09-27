bc32dc4b69f8e6e918927a4f4379effdc98989f4
Ashutosh Sharma <mr.proashutosh@gmail.com>
2025-09-28 00:02:34 +0530

    main.jsx fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { AuthProvider } from './context/AuthContext.tsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
-);
-
+    <App />
+  </React.StrictMode>,
+)
